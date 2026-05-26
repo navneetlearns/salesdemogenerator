60ebc541c0c01da42e90ff608c023fb156538ab6
Navneet Siwan <navneetsiwan@gmail.com>
2026-05-26 13:29:23 +0000

    chore: remove debug test endpoint, api/generate now working

--- api/test-req.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  const results = {};
-  try { require('../runtime/generate-session'); results.m = 'OK'; }
-  catch(e) { results.m = e.message; results.stack = e.stack; }
-  res.end(JSON.stringify(results));
-};
